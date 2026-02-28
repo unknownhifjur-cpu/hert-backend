99770c3085d156e1e27c8f1bbe7d53003f7ba097
Hifjur Alam <unknownhifjur@gmail.com>
2026-03-01 02:15:41 +0530

    correction of bond model

--- models/Bond.js
+++ models/Bond.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const arrayLimit = (val) => val.length === 2;
 
 const BondSchema = new mongoose.Schema({
-  users: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    validate: [arrayLimit, '{PATH} must have exactly 2 users']
-  }],
+  users: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }],
+    validate: [arrayLimit, 'Users array must have exactly 2 users']
+  },
   bondData: {
     type: mongoose.Schema.Types.Mixed,
     default: () => ({
